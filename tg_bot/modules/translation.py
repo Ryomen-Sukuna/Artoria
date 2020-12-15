bf07ff393bb29de3ddca282c3ff12d6458044932
Hyper <70685160+hyper-ub@users.noreply.github.com>
2020-12-15 22:18:56 +0530

    Update translation.py

--- tg_bot/modules/translation.py
+++ tg_bot/modules/translation.py
@@ -1,59 +1,75 @@
-import json
-from pprint import pprint
-
+from typing import Optional, List
+from gtts import gTTS
+import os
 import requests
-from telegram import Update, Bot
-from telegram.ext import CommandHandler
+import json
+from emoji import UNICODE_EMOJI
+
+from telegram import ChatAction
+from telegram.ext import run_async
 
 from tg_bot import dispatcher
+from tg_bot.modules.disable import DisableAbleCommandHandler
+from tg_bot.modules.helper_funcs.alternate import typing_action, send_action
+
+@run_async
+@send_action(ChatAction.RECORD_AUDIO)
+def gtts(update, context):
+    msg = update.effective_message
+    reply = " ".join(context.args)
+    if not reply:
+        if msg.reply_to_message:
+            reply = msg.reply_to_message.text
+        else:
+            return msg.reply_text(
+                "Reply to some message or enter some text to convert it into audio format!"
+            )
+        for x in "\n":
+            reply = reply.replace(x, "")
+    try:
+        tts = gTTS(reply)
+        tts.save("k.mp3")
+        with open("k.mp3", "rb") as speech:
+            msg.reply_audio(speech)
+    finally:
+        if os.path.isfile("k.mp3"):
+            os.remove("k.mp3")
+
 
 # Open API key
 API_KEY = "6ae0c3a0-afdc-4532-a810-82ded0054236"
 URL = "http://services.gingersoftware.com/Ginger/correct/json/GingerTheText"
 
 
-def translate(bot: Bot, update: Update):
+@run_async
+@typing_action
+def spellcheck(update, context):
     if update.effective_message.reply_to_message:
         msg = update.effective_message.reply_to_message
 
-        params = dict(
-            lang="US",
-            clientVersion="2.0",
-            apiKey=API_KEY,
-            text=msg.text
-        )
+        params = dict(lang="US", clientVersion="2.0", apiKey=API_KEY, text=msg.text)
 
         res = requests.get(URL, params=params)
-        # print(res)
-        # print(res.text)
-        pprint(json.loads(res.text))
-        changes = json.loads(res.text).get('LightGingerTheTextResult')
+        changes = json.loads(res.text).get("LightGingerTheTextResult")
         curr_string = ""
-
         prev_end = 0
 
         for change in changes:
-            start = change.get('From')
-            end = change.get('To') + 1
-            suggestions = change.get('Suggestions')
+            start = change.get("From")
+            end = change.get("To") + 1
+            suggestions = change.get("Suggestions")
             if suggestions:
-                sugg_str = suggestions[0].get('Text')  # should look at this list more
+                sugg_str = suggestions[0].get("Text")  # should look at this list more
                 curr_string += msg.text[prev_end:start] + sugg_str
-
                 prev_end = end
 
         curr_string += msg.text[prev_end:]
-        print(curr_string)
         update.effective_message.reply_text(curr_string)
+    else:
+        update.effective_message.reply_text(
+            "Reply to some message to get grammar corrected text!"
+        )
 
-
-__help__ = """
- - /t: while replying to a message, will reply with a grammar corrected version
-"""
-
-__mod_name__ = "Translator"
-
-
-TRANSLATE_HANDLER = CommandHandler('t', translate)
-
-dispatcher.add_handler(TRANSLATE_HANDLER)
+dispatcher.add_handler(DisableAbleCommandHandler(["tr", "tl"], gtrans, pass_args=True))
+dispatcher.add_handler(DisableAbleCommandHandler("tts", gtts, pass_args=True))
+dispatcher.add_handler(DisableAbleCommandHandler("splcheck", spellcheck))
