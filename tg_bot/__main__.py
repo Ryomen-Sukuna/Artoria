36fb19376b96c705fadde633ab84a53957d24e39
devil154 <74478607+devil154@users.noreply.github.com>
2020-12-16 00:19:58 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -21,8 +21,8 @@ from tg_bot import (dispatcher, since_time_start, updater, TOKEN, OWNER_ID, WEBH
 # NOTE: Module order is not guaranteed, specify that in the config file!
 from tg_bot.modules import ALL_MODULES
 from tg_bot.modules.purge import client
-from tg_bog.modules.helper_funcs.chat_status import is_user_admin
-from tg_boy.modules.helper_funcs.misc import paginate_modules
+from tg_bot.modules.helper_funcs.chat_status import is_user_admin
+from tg_bot.modules.helper_funcs.misc import paginate_modules
 from tg_bot.modules.helper_funcs.alternate import typing_action
 
 
