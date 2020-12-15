322883ca74ae23d4de965658dbab9b633ddc4fb9
Hyper <70685160+hyper-ub@users.noreply.github.com>
2020-12-15 21:35:32 +0530

    Update info.py

--- tg_bot/modules/info.py
+++ tg_bot/modules/info.py
@@ -30,93 +30,6 @@ import tg_bot.modules.helper_funcs.cas_api as cas
 OFFICERS = [OWNER_ID] + DEV_USERS + SUDO_USERS 
 
 
-#HELTH-BAR - Show User Health -- This Feature From @SaitamaRobot
-def no_by_per(totalhp, percentage):
-    """
-    rtype: num of `percentage` from total
-    eg: 1000, 10 -> 10% of 1000 (100)
-    """
-    return totalhp * percentage / 100
-
-
-def get_percentage(totalhp, earnedhp):
-    """
-    rtype: percentage of `totalhp` num
-    eg: (1000, 100) will return 10%
-    """
-
-    matched_less = totalhp - earnedhp
-    per_of_totalhp = 100 - matched_less * 100.0 / totalhp
-    per_of_totalhp = str(int(per_of_totalhp))
-    return per_of_totalhp
-
-
-def hpmanager(user):
-    total_hp = (get_user_num_chats(user.id) + 10) * 10
-
-    if not is_user_gbanned(user.id):
-
-        # Assign new var `new_hp` since we need `total_hp` in
-        # end to calculate percentage.
-        new_hp = total_hp
-
-        # if no username decrease 25% of hp.
-        if not user.username:
-            new_hp -= no_by_per(total_hp, 25)
-        try:
-            dispatcher.bot.get_user_profile_photos(user.id).photos[0][-1]
-        except IndexError:
-            # no profile photo ==> -25% of hp
-            new_hp -= no_by_per(total_hp, 25)
-        # if no /setme exist ==> -20% of hp
-        if not sql.get_user_me_info(user.id):
-            new_hp -= no_by_per(total_hp, 20)
-        # if no bio exsit ==> -10% of hp
-        if not sql.get_user_bio(user.id):
-            new_hp -= no_by_per(total_hp, 10)
-
-        if is_user_afk(user.id):
-            afkst = afk_reason(user.id)
-            # if user is afk and no reason then decrease 7%
-            # else if reason exist decrease 5%
-            if not afkst:
-                new_hp -= no_by_per(total_hp, 7)
-            else:
-                new_hp -= no_by_per(total_hp, 5)
-
-        # fbanned users will have (2*number of fbans) less from max HP
-        # Example: if HP is 100 but user has 5 diff fbans
-        # Available HP is (2*5) = 10% less than Max HP
-        # So.. 10% of 100HP = 90HP
-
-        _, fbanlist = get_user_fbanlist(user.id)
-        new_hp -= no_by_per(total_hp, 2 * len(fbanlist))
-
-    # Bad status effects:
-    # gbanned users will always have 5% HP from max HP
-    # Example: If HP is 100 but gbanned
-    # Available HP is 5% of 100 = 5HP
-
-    else:
-        new_hp = no_by_per(total_hp, 5)
-
-    return {
-        "earnedhp": int(new_hp),
-        "totalhp": int(total_hp),
-        "percentage": get_percentage(total_hp, new_hp)
-    }
-
-
-def make_bar(per):
-    done = min(round(per / 10), 10)
-    return "█" * done + "▓" * (10 - done)
-
-
-
-
-
-
-#information
 @run_async
 def info(update: Update, context: CallbackContext):
     bot, args = context.bot, context.args
@@ -201,11 +114,6 @@ def info(update: Update, context: CallbackContext):
                 text += f"\n∘ Admin Title: <code>{custom_title}</code> \n"
     except BadRequest:
         pass
-
-    if user_id not in [bot.id, 777000, 1087968824]:
-        userhp = hpmanager(user)
-        text += f"\n∘ Health : <code>{userhp['earnedhp']}/{userhp['totalhp']}</code> ∙ <code>{userhp['percentage']}% </code> \n( {make_bar(int(userhp['percentage']))} ) \n "                                                                                         
-
    
     if user.id ==1286562476:
         text += "\n🚶🏻‍♂️Uff,This person is sudo \n HE IS is the cutie!."
