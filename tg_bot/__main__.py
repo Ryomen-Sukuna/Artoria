376cb5d37d22b11a5b3021e266dca8e358841678
Hyper <70685160+hyper-ub@users.noreply.github.com>
2020-12-16 14:11:36 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -603,19 +603,13 @@ def main():
             updater.bot.set_webhook(url=URL + TOKEN, certificate=open(CERT_PATH, "rb"))
         else:
             updater.bot.set_webhook(url=URL + TOKEN)
-            
+            client.run_until_disconnected()
 
     else:
         LOGGER.info("Using long polling.")
         updater.start_polling(timeout=15, read_latency=4)
-        
-    if len(argv) not in (1, 3, 4):
-        client.disconnect()
-    else:
         client.run_until_disconnected()
 
- 
-    
     updater.idle()
 
 
