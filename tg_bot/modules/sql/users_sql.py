7fca52abf2acd63b8d02fc26f30623882ab1853c
devil154 <74478607+devil154@users.noreply.github.com>
2020-12-16 07:12:55 +0530

    Update users_sql.py

--- tg_bot/modules/sql/users_sql.py
+++ tg_bot/modules/sql/users_sql.py
@@ -172,6 +172,13 @@ def num_chats():
     finally:
         SESSION.close()
 
+def get_user_com_chats(user_id):
+    try:
+        chat_members = SESSION.query(ChatMembers).filter(
+            ChatMembers.user == int(user_id)).all()
+        return [i.chat for i in chat_members]
+    finally:
+        SESSION.close()
 
 def num_users():
     try:
