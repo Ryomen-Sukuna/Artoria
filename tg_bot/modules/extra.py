65402217623928686555a22b93fed97decf5df39
Hyper <70685160+hyper-ub@users.noreply.github.com>
2020-12-17 22:14:50 +0530

    Update extra.py

--- tg_bot/modules/extra.py
+++ tg_bot/modules/extra.py
@@ -124,6 +124,8 @@ def covid(update: Update, context: CallbackContext):
     message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN)
 
 
+__help__ = f"""
+
  - /covid :To get Global data
  - /covid` <country>:To get data of a country
  - /cash : currency converter Example syntax: /cash 1 USD 
