8eec52c7e98bb75f42ab98181ae7bed816730877
Hyper <70685160+hyper-ub@users.noreply.github.com>
2020-12-17 22:47:28 +0530

    Update help.py

--- tg_bot/modules/help.py
+++ tg_bot/modules/help.py
@@ -1,10 +1,10 @@
-__mod_name__ = "Extra"    
+__mod_name__ = "Tools"    
 
 __help__ = """ 
 
  - /covid :To get Global data
  - /covid` <country>:To get data of a country
  - /cash : currency converter Example syntax: /cash 1 USD 
- - /app 
+ - /app  : search app on Google Play Store
 
 """    
