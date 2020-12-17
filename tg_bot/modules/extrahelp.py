3a664eb043fb6a6e48aa39729882293df07b330d
Hyper <70685160+hyper-ub@users.noreply.github.com>
2020-12-17 22:43:24 +0530

    Delete extrahelp.py

--- tg_bot/modules/extrahelp.py
+++
@@ -1,9 +0,0 @@
-__help__ = """
-
- - /covid :To get Global data
- - /covid` <country>:To get data of a country
- - /cash : currency converter Example syntax: /cash 1 USD 
- - /app <app name> : finds an app in playstore for you
-
- """
-__mod_name__ = "Extras"
