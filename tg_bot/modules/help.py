6930a664a2dbe2b3503ba2ce95795c24c844f6a4
Hyper <70685160+hyper-ub@users.noreply.github.com>
2020-12-17 22:43:03 +0530

    Update and rename human.py to help.py

---
+++ tg_bot/modules/help.py
@@ -0,0 +1,10 @@
+__mod_name__ = "Extra"    
+
+__help__ = """ 
+
+ - /covid :To get Global data
+ - /covid` <country>:To get data of a country
+ - /cash : currency converter Example syntax: /cash 1 USD 
+ - /app 
+
+"""    
