9be649ed03b994e1b9757e72ba7305c0819237bf
Hyper <70685160+hyper-ub@users.noreply.github.com>
2020-12-17 22:37:44 +0530

    Create human.py

---
+++ tg_bot/modules/human.py
@@ -0,0 +1,14 @@
+__mod_name__ = "Tagger"    
+
+__help__ = """ 
+Tagger is an essential feature to mention all subscribed members in the group. Any chat members can subscribe to tagger.
+
+- /tagme: registers to the chat tag list.
+- /untagme: unsubscribes from the chat tag list.
+
+*Admin only:*
+- /tagall: mention all subscribed members.
+- /untagall: clears all subscribed members. 
+- /addtag <userhandle>: add a user to chat tag list. (via handle, or reply)
+- /removetag <userhandle>: remove a user to chat tag list. (via handle, or reply)
+"""    
