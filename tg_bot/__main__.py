2cfd71b4a0cb9a83b7bb41093e4313b9bc928450
Hyper <70685160+hyper-ub@users.noreply.github.com>
2020-12-18 10:26:16 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -175,16 +175,16 @@ def start(update, context):
                             url="https://t.me/saber_herobot?start=help")
                     ]]))
        
-@run_async
-def tg_bot_about_callback(update, context):
-    query = update.callback_query
-    if query.data == "home_back":
-        query.message.edit_text(
-                PM_START_TEXT,
-                parse_mode=ParseMode.MARKDOWN,
-                reply_markup=InlineKeyboardMarkup(buttons),
-                timeout=60, 
-            )
+#@run_async
+#def tg_bot_about_callback(update, context):
+    #query = update.callback_query
+    #if query.data == "home_back":
+        #query.message.edit_text(
+                #PM_START_TEXT,
+                #parse_mode=ParseMode.MARKDOWN,
+                #reply_markup=InlineKeyboardMarkup(buttons),
+                #timeout=60,)
+            
                                                                  
                                                                  
 
@@ -587,7 +587,7 @@ def main():
     
     settings_handler = CommandHandler("settings", get_settings)
     settings_callback_handler = CallbackQueryHandler(settings_button, pattern=r"stngs_")
-    about_callback_handler = CallbackQueryHandler(tg_bot_about_callback, pattern=r"home_back")
+    ₹about_callback_handler = CallbackQueryHandler(tg_bot_about_callback, pattern=r"home_back")
 
     
     migrate_handler = MessageHandler(Filters.status_update.migrate, migrate_chats)
@@ -603,7 +603,7 @@ def main():
     dispatcher.add_handler(settings_callback_handler)
     dispatcher.add_handler(migrate_handler)
     dispatcher.add_handler(is_chat_allowed_handler)
-    dispatcher.add_handler(about_callback_handler)
+    #dispatcher.add_handler(about_callback_handler)
     dispatcher.add_error_handler(error_handler)
 
     if WEBHOOK:
