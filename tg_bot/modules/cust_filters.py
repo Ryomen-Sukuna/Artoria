1db03fc30b24ca8f7be87146e20f534292d6f2c3
Hyper <70685160+hyper-ub@users.noreply.github.com>
2020-12-19 06:42:35 +0530

    Update cust_filters.py

--- tg_bot/modules/cust_filters.py
+++ tg_bot/modules/cust_filters.py
@@ -16,7 +16,7 @@ from telegram.ext import (
 from telegram.utils.helpers import mention_html, escape_markdown
 
 from tg_bot import dispatcher, LOGGER, SUDO_USERS
-from tg_bot.disable import DisableAbleCommandHandler
+from tg_bot.modules.disable import DisableAbleCommandHandler
 from tg_bot.modules.helper_funcs.handlers import MessageHandlerChecker
 from tg_bot.modules.helper_funcs.chat_status import user_admin
 from tg_bot.modules.helper_funcs.extraction import extract_text
