468e7a311b2aba16889d9be98c9ee49b788041a1
Hyper <70685160+hyper-ub@users.noreply.github.com>
2020-12-19 07:18:56 +0530

    Update chat_status.py

--- tg_bot/modules/helper_funcs/chat_status.py
+++ tg_bot/modules/helper_funcs/chat_status.py
@@ -15,12 +15,12 @@ def can_delete(chat: Chat, bot_id: int) -> bool:
 
 
 def is_user_ban_protected(chat: Chat, user_id: int, member: ChatMember = None) -> bool:
-    if (
-        chat.type == "private"
-        or user_id in SUDO_USERS
-        or user_id in WHITELIST_USERS
-        or chat.all_members_are_administrators
-    ):
+    if chat.type == 'private' \
+            or user_id in SUDO_USERS \
+            or user_id in DEV_USERS \
+            or user_id in WHITELIST_USERS \
+            or user_id in [777000, 1087968824]\
+            or chat.all_members_are_administrators:
         return True
 
     if not member:
@@ -30,12 +30,11 @@ def is_user_ban_protected(chat: Chat, user_id: int, member: ChatMember = None) -
 
 @MWT(timeout=60 * 5)  # Cache admin status for 5 mins to avoid extra requests.
 def is_user_admin(chat: Chat, user_id: int, member: ChatMember = None) -> bool:
-    if (
-        chat.type == "private"
-        or user_id in SUDO_USERS
-        or user_id == int(777000)
-        or chat.all_members_are_administrators
-    ):
+    if chat.type == 'private' \
+            or user_id in SUDO_USERS \
+            or user_id in DEV_USERS \
+            or user_id in [777000, 1087968824] \
+            or chat.all_members_are_administrators:
         return True
 
     if not member:
