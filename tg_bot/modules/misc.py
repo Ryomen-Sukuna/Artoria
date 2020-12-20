7b5175c46fda4048dd1a475d7577b9705a2f723e
Hyper <70685160+hyper-ub@users.noreply.github.com>
2020-12-20 22:19:50 +0530

    Update misc.py

--- tg_bot/modules/misc.py
+++ tg_bot/modules/misc.py
@@ -526,9 +526,8 @@ def reply_keyboard_remove(update, context):
 
 @run_async
 def stats(update, context):
-    stats = f"┎─⌈ <b>Current {dispatcher.bot.first_name} Stats</b> ⌋\n┇\n" + "\n┋\n".join([mod.__stats__() for mod in STATS])
-    result = re.sub(r'(\d+)', r'<code>\1</code>', stats)
-    update.effective_message.reply_text(result, parse_mode=ParseMode.HTML)
+    update.effective_message.reply_text("Current stats:\n" + "\n".join([mod.__stats__() for mod in STATS]))
+
 
 
 # /ip is for private use
