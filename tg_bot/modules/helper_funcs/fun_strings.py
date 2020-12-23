81fb2199297f77c72777b55874f8d6e93a93661c
Hyper <70685160+hyper-ub@users.noreply.github.com>
2020-12-23 10:47:54 +0530

    Update fun_strings.py

--- tg_bot/modules/helper_funcs/fun_strings.py
+++ tg_bot/modules/helper_funcs/fun_strings.py
@@ -568,26 +568,13 @@ GBAM = "<b>Beginning Of Global Bam For {user2}</b>  \n \nChat Id : <code>{chatid
 
 
 GBAM_REASON = (
-    "sasta noob",
-    "sasta waifu stealer",
-    "sasta white-het hekur",
-    "sasta white-het codur",
-    "sasta white-het vala chintu",
-    "sasta hexa hekur",
-    "sasta hexa playur",
-    "sasta tiktokur💃🏾",
-    "sasta membor of team 7",
-    "sasta selmon boi",
-    "sasta fri-fire player",
-    "sasta chhota bhim",
-    "sasta chhapri",
-    "sasta jony sin",
-    "sasta chhapri nibba",
-    "sasta nibba",
-    "sasti ria chokroborti",
-    "sasti nibbi",
-    "sasta camper",
-
+    "noob",
+    "Scammer",
+    "Shit of telegram",
+    "Official gay of tg",    
+    "Official pro of tg ",
+    "Waifu stealer"
+    "Nibha",
 )
 
 
