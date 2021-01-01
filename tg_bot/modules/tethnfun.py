6779bdd0192766872771b0af6f851d6e5b3f53ad
Hyper <70685160+hyper-ub@users.noreply.github.com>
2021-01-01 15:34:05 +0530

    Update tethnfun.py

--- tg_bot/modules/tethnfun.py
+++ tg_bot/modules/tethnfun.py
@@ -7,6 +7,17 @@ from tg_bot.events import register
 from tg_bot import client
 import sys
 import shutil
+import glob
+from re import findall
+from bing_image_downloader import downloader
+from telethon import *
+from telethon.tl import functions
+from telethon.tl import types
+from telethon.tl.types import *
+
+from tg_bot import oko
+from tg_bot.events import register
+
 from re import findall
 from telethon import *
 from telethon.tl import functions
@@ -68,6 +79,25 @@ async def _(event):
         output_str += "[{}]({})\n{}\n".format(text, url, last)       
     await event.reply("{}".format(output_str), link_preview=False, parse_mode='Markdown')
 
+@register(pattern="^/img (.*)")
+async def img_sampler(event):
+     if event.fwd_from:
+        return
+     if event.is_group:
+       if not (await is_register_admin(event.input_chat, event.message.sender_id)):
+          await event.reply(".. You are not admin.. use in bot  pm")
+          return
+     query = event.pattern_match.group(1)
+     jit = f'"{query}"'
+     downloader.download(jit, limit=5, output_dir='store', adult_filter_off=False, force_replace=False, timeout=60)
+     os.chdir(f'./store/"{query}"')
+     types = ('*.png', '*.jpeg', '*.jpg') # the tuple of file types
+     files_grabbed = []
+     for files in types:
+         files_grabbed.extend(glob.glob(files))
+     await event.client.send_file(event.chat_id, files_grabbed, reply_to=event.id)
+     os.remove(files_grabbed)
+     os.chdir('./')
 
 @register(pattern=r"^/getqr$")
 async def parseqr(qr_e):
