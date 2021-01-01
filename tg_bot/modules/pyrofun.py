eba1874a68f90dc6d8d74f0d6708dd9acdb14a41
Hyper <70685160+hyper-ub@users.noreply.github.com>
2021-01-01 11:34:47 +0530

    Update pyrofun.py

--- tg_bot/modules/pyrofun.py
+++ tg_bot/modules/pyrofun.py
@@ -13,6 +13,54 @@ from tg_bot import TOKEN, OWNER_ID ,SUDO_USERS, pbot
 DART_E_MOJI = "🎯"
 FOOTBALL_E_MOJI="⚽"
 
+def yt_search(song):
+    videosSearch = VideosSearch(song, limit=1)
+    result = videosSearch.result()
+    if not result:
+        return False
+    else:
+        video_id = result["result"][0]["id"]
+        url = f"https://youtu.be/{video_id}"
+        return url
+
+
+@pbot.on_message(filters.command("song"))
+async def song(client, message):
+    chat_id = message.chat.id
+    user_id = message.from_user["id"]
+
+    args = get_arg(message) + " " + "song"
+    if args.startswith(" "):
+        await message.reply("Enter a song name. Check /help")
+        return ""
+    status = await message.reply("Processing...")
+    video_link = yt_search(args)
+    if not video_link:
+        await status.edit("Song not found.")
+        return ""
+    yt = YouTube(video_link)
+    audio = yt.streams.filter(only_audio=True).first()
+    try:
+        download = audio.download(filename=f"{str(user_id)}")
+    except Exception as ex:
+        await status.edit("Failed to download song")
+        LOGGER.error(ex)
+        return ""
+    rename = os.rename(download, f"{str(user_id)}.mp3")
+    await app.send_chat_action(message.chat.id, "upload_audio")
+    await app.send_audio(
+        chat_id=message.chat.id,
+        audio=f"{str(user_id)}.mp3",
+        duration=int(yt.length),
+        title=str(yt.title),
+        performer=str(yt.author),
+        reply_to_message_id=message.message_id,
+    )
+    await status.delete()
+    os.remove(f"{str(user_id)}.mp3")
+
+
+
 @pbot.on_message(filters.command('basket'))
 async def basket(c: Client, m: Message):
     await c.send_dice(m.chat.id, reply_to_message_id=m.message_id, emoji="🏀")
