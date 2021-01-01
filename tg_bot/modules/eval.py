362ac96fffb2014ef47c2f13def53cfd2cc8c018
Hyper <70685160+hyper-ub@users.noreply.github.com>
2021-01-01 12:20:43 +0530

    Update eval.py

--- tg_bot/modules/eval.py
+++ tg_bot/modules/eval.py
@@ -1,65 +1,137 @@
 import io
-import sys
+import os
+# Common imports for eval
+import textwrap
 import traceback
-from tg_bot import pbot
-from pyrogram import Client, filters
-from tg_bot import OWNER_ID
+from contextlib import redirect_stdout
+
+from tg_bot import LOGGER, dispatcher
+from tg_bot.modules.helper_funcs.chat_status import dev_plus
+from telegram import ParseMode, Update
+from telegram.ext import CallbackContext, CommandHandler, run_async
+
+namespaces = {}
 
 
-@pbot.on_message(filters.user(OWNER_ID) & filters.command("eval"))
-async def eval(client, message):
-    status_message = await message.reply_text("Processing ...")
-    cmd = message.text.split(" ", maxsplit=1)[1]
+def namespace_of(chat, update, bot):
+    if chat not in namespaces:
+        namespaces[chat] = {
+            '__builtins__': globals()['__builtins__'],
+            'bot': bot,
+            'effective_message': update.effective_message,
+            'effective_user': update.effective_user,
+            'effective_chat': update.effective_chat,
+            'update': update
+        }
 
-    reply_to_ = message
-    if message.reply_to_message:
-        reply_to_ = message.reply_to_message
+    return namespaces[chat]
 
-    old_stderr = sys.stderr
-    old_stdout = sys.stdout
-    redirected_output = sys.stdout = io.StringIO()
-    redirected_error = sys.stderr = io.StringIO()
-    stdout, stderr, exc = None, None, None
+
+def log_input(update):
+    user = update.effective_user.id
+    chat = update.effective_chat.id
+    LOGGER.info(
+        f"IN: {update.effective_message.text} (user={user}, chat={chat})")
+
+
+def send(msg, bot, update):
+    if len(str(msg)) > 2000:
+        with io.BytesIO(str.encode(msg)) as out_file:
+            out_file.name = "output.txt"
+            bot.send_document(
+                chat_id=update.effective_chat.id, document=out_file)
+    else:
+        LOGGER.info(f"OUT: '{msg}'")
+        bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=f"`{msg}`",
+            parse_mode=ParseMode.MARKDOWN)
+
+
+@dev_plus
+@run_async
+def evaluate(update: Update, context: CallbackContext):
+    bot = context.bot
+    send(do(eval, bot, update), bot, update)
+
+
+@dev_plus
+@run_async
+def execute(update: Update, context: CallbackContext):
+    bot = context.bot
+    send(do(exec, bot, update), bot, update)
+
+
+def cleanup_code(code):
+    if code.startswith('```') and code.endswith('```'):
+        return '\n'.join(code.split('\n')[1:-1])
+    return code.strip('` \n')
+
+
+def do(func, bot, update):
+    log_input(update)
+    content = update.message.text.split(' ', 1)[-1]
+    body = cleanup_code(content)
+    env = namespace_of(update.message.chat_id, update, bot)
+
+    os.chdir(os.getcwd())
+    with open(
+            os.path.join(os.getcwd(),
+                         'SaitamaRobot/modules/helper_funcs/temp.txt'),
+            'w') as temp:
+        temp.write(body)
+
+    stdout = io.StringIO()
+
+    to_compile = f'def func():\n{textwrap.indent(body, "  ")}'
 
     try:
-        await aexec(cmd, client, message)
-    except Exception:
-        exc = traceback.format_exc()
+        exec(to_compile, env)
+    except Exception as e:
+        return f'{e.__class__.__name__}: {e}'
 
-    stdout = redirected_output.getvalue()
-    stderr = redirected_error.getvalue()
-    sys.stdout = old_stdout
-    sys.stderr = old_stderr
+    func = env['func']
 
-    evaluation = ""
-    if exc:
-        evaluation = exc
-    elif stderr:
-        evaluation = stderr
-    elif stdout:
-        evaluation = stdout
+    try:
+        with redirect_stdout(stdout):
+            func_return = func()
+    except Exception as e:
+        value = stdout.getvalue()
+        return f'{value}{traceback.format_exc()}'
     else:
-        evaluation = "Success"
-
-    final_output = "<b>EVAL</b>: "
-    final_output += f"<code>{cmd}</code>\n\n"
-    final_output += "<b>OUTPUT</b>:\n"
-    final_output += f"<code>{evaluation.strip()}</code> \n"
-
-    if len(final_output) > 4096:
-        with io.BytesIO(str.encode(final_output)) as out_file:
-            out_file.name = "eval.text"
-            await reply_to_.reply_document(
-                document=out_file, caption=cmd, disable_notification=True
-            )
-    else:
-        await reply_to_.reply_text(final_output)
-    await status_message.delete()
+        value = stdout.getvalue()
+        result = None
+        if func_return is None:
+            if value:
+                result = f'{value}'
+            else:
+                try:
+                    result = f'{repr(eval(body, env))}'
+                except:
+                    pass
+        else:
+            result = f'{value}{func_return}'
+        if result:
+            return result
 
 
-async def aexec(code, client, message):
-    exec(
-        "async def __aexec(client, message): "
-        + "".join(f"\n {l_}" for l_ in code.split("\n"))
-    )
-    return await locals()["__aexec"](client, message)
+@dev_plus
+@run_async
+def clear(update: Update, context: CallbackContext):
+    bot = context.bot
+    log_input(update)
+    global namespaces
+    if update.message.chat_id in namespaces:
+        del namespaces[update.message.chat_id]
+    send("Cleared locals.", bot, update)
+
+
+EVAL_HANDLER = CommandHandler(('e', 'ev', 'eva', 'eval'), evaluate)
+EXEC_HANDLER = CommandHandler(('x', 'ex', 'exe', 'exec', 'py'), execute)
+CLEAR_HANDLER = CommandHandler('clearlocals', clear)
+
+dispatcher.add_handler(EVAL_HANDLER)
+dispatcher.add_handler(EXEC_HANDLER)
+dispatcher.add_handler(CLEAR_HANDLER)
+
+__mod_name__ = "Eval Module"
