98eaf777ad3bfbd4f80acced6e093c31d3a79298
Hyper <70685160+hyper-ub@users.noreply.github.com>
2021-01-01 12:46:27 +0530

    Update chat_status.py

--- tg_bot/modules/helper_funcs/chat_status.py
+++ tg_bot/modules/helper_funcs/chat_status.py
@@ -35,23 +35,9 @@ def is_user_admin(chat: Chat, user_id: int, member: ChatMember = None) -> bool:
             user_id in [777000, 1087968824
                        ]):  # Count telegram and Group Anonymous as admin
         return True
-
     if not member:
-        with THREAD_LOCK:
-            # try to fetch from cache first.
-            try:
-                return user_id in ADMIN_CACHE[chat.id]
-            except KeyError:
-                # keyerror happend means cache is deleted,
-                # so query bot api again and return user status
-                # while saving it in cache for future useage...
-                chat_admins = dispatcher.bot.getChatAdministrators(chat.id)
-                admin_list = [x.user.id for x in chat_admins]
-                ADMIN_CACHE[chat.id] = admin_list
-
-                if user_id in admin_list:
-                    return True
-                return False
+        member = chat.get_member(user_id)
+    return member.status in ("administrator", "creator")
 
 def is_bot_admin(chat: Chat, bot_id: int, bot_member: ChatMember = None) -> bool:
     if chat.type == "private" or chat.all_members_are_administrators:
