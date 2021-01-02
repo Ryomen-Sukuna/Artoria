c8769d07ab4542067968414aa43d89b28bc7bfa9
Hyper <70685160+hyper-ub@users.noreply.github.com>
2021-01-02 07:50:04 +0530

    Update pyrofun.py

--- tg_bot/modules/pyrofun.py
+++ tg_bot/modules/pyrofun.py
@@ -1,4 +1,5 @@
-
+import speedtest
+import re
 import html
 import regex
 import os
@@ -7,7 +8,11 @@ from youtubesearchpython import VideosSearch
 from tg_bot.utils.ut import get_arg
 from tg_bot import pbot, LOGGER
 from pyrogram import Client, filters
-from pyrogram.types import Message
+from pyrogram.types import Messages
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from tg_bot import OWNER_ID, pbot
+
+
 
 DART_E_MOJI = "🎯"
 FOOTBALL_E_MOJI="⚽"
@@ -22,6 +27,64 @@ def yt_search(song):
         url = f"https://youtu.be/{video_id}"
         return url
 
+
+def convert(speed):
+    return round(int(speed) / 1048576, 2)
+
+
+async def speedtestxyz(client, message):
+    buttons = [[InlineKeyboardButton("Image",
+                                    callback_data="speedtest_image"),
+                InlineKeyboardButton("Text",
+                                    callback_data="speedtest_text")]]
+    await message.reply_text(
+        "Select SpeedTest Mode",
+        reply_markup=InlineKeyboardMarkup(buttons))
+
+
+def speed_convert(size):
+    """Hi human, you can't read bytes?"""
+    power = 2 ** 10
+    zero = 0
+    units = {0: '', 1: 'Kb/s', 2: 'Mb/s', 3: 'Gb/s', 4: 'Tb/s'}
+    while size > power:
+        size /= power
+        zero += 1
+    return f"{round(size, 2)} {units[zero]}"
+
+
+def speedtest_callback(_, __, query):
+    if re.match("speedtest", query.data):
+        return True
+
+
+speedtest_create = filters.create(speedtest_callback)
+
+
+@pbot.on_callback_query(speedtest_create)
+async def speedtestxyz_callback(client, query):
+    if query.from_user.id in OWNER_ID:
+        await query.message.edit_text('Runing a speedtest....')
+        speed = speedtest.Speedtest()
+        speed.get_best_server()
+        speed.download()
+        speed.upload()
+        replymsg = 'SpeedTest Results:'
+
+        if query.data == 'speedtest_image':
+            speedtest_image = speed.results.share()
+            replym = f"**[SpeedTest Results:]({speedtest_image})**"
+            await query.message.edit_text(replym, parse_mode="markdown")
+
+        elif query.data == 'speedtest_text':
+            result = speed.results.dict()
+            replymsg += f"\n - **ISP:** `{result['client']['isp']}`"
+            replymsg += f"\n - **Download:** `{speed_convert(result['download'])}`"
+            replymsg += f"\n - **Upload:** `{speed_convert(result['upload'])}`"
+            replymsg += f"\n - **Ping:** `{result['ping']}`"
+            await query.message.edit_text(replymsg, parse_mode="markdown")
+    else:
+        await client.answer_callback_query(query.id, "No, you are not allowed to do this", show_alert=False)
 
 @pbot.on_message(filters.command("song"))
 async def song(client, message):
