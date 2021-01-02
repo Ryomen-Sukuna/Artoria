cae3bc7df680bdaa8a99525577021d775603c5ec
Hyper <70685160+hyper-ub@users.noreply.github.com>
2021-01-02 15:22:57 +0530

    Update info.py

--- tg_bot/modules/info.py
+++ tg_bot/modules/info.py
@@ -60,8 +60,9 @@ def info(update: Update, context: CallbackContext):
 
     if user.username:
         text += f"\n∘ Username: @{html.escape(user.username)}"
-
     
+    text += f"\nPermalink: {mention_html(user.id, 'link')}"
+
     isafk = is_user_afk(user.id)
     try:
         text += "\n\n∘ Currently AFK: "
