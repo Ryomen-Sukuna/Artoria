be40012430d513c872d1b26be7d135114e8c2548
Hyper <70685160+hyper-ub@users.noreply.github.com>
2021-01-04 13:28:59 +0530

    Update afk.py

--- tg_bot/modules/afk.py
+++ tg_bot/modules/afk.py
@@ -182,7 +182,6 @@ __mod_name__ = "AFK"
 __command_list__ = ["afk"]
 __handlers__ = [
     (AFK_HANDLER, AFK_GROUP),
-    (AFK_REGEX_HANDLER, AFK_GROUP),
     (NO_AFK_HANDLER, AFK_GROUP),
     (AFK_REPLY_HANDLER, AFK_REPLY_GROUP),
 ]
