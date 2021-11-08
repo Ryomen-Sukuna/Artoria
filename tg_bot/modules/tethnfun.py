2ab2aee81d70d7c0723ef9930553d9a52c627ee7
ZERO • アクバル <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-11-09 02:54:44 +0700

    PYL-W0621

--- tg_bot/modules/tethnfun.py
+++ tg_bot/modules/tethnfun.py
@@ -94,9 +94,9 @@ async def img_sampler(event):
         timeout=60,
     )
     os.chdir(f'./store/"{query}"')
-    types = ("*.png", "*.jpeg", "*.jpg")  # the tuple of file types
+    zero = ("*.png", "*.jpeg", "*.jpg")  # the tuple of file types
     files_grabbed = []
-    for files in types:
+    for files in zero:
         files_grabbed.extend(glob.glob(files))
     await event.client.send_file(event.chat_id, files_grabbed, reply_to=event.id)
     os.remove(files_grabbed)
